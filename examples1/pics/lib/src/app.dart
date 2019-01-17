7209de3f0a36d505e18e8c0bd13e4d6c56a21f50
Haythem Sellami <sellamihaythem96@gmail.com>
2019-01-17 11:56:04 +0100

    model + http request + image widget

--- examples1/pics/lib/src/app.dart
+++ examples1/pics/lib/src/app.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import 'dart:convert';
+import 'models/image_model.dart';
+import 'widgets/image_list.dart';
 
 class App extends StatefulWidget {
   @override
@@ -9,6 +13,17 @@ class App extends StatefulWidget {
 
 class AppState extends State<App> {
   int counter = 0;
+  List<ImageModel> images = [];
+
+  void fetchImage() async {
+    counter++;
+    var response = await get("http://jsonplaceholder.typicode.com/photos/$counter");
+    var imageModel = ImageModel.fromJson(json.decode(response.body));
+    //update state
+    setState(() {
+      images.add(imageModel);   
+    });
+  }
 
   Widget build(context) {
     return MaterialApp(
@@ -16,14 +31,10 @@ class AppState extends State<App> {
         appBar: AppBar(
           title: Text("let's see some images"),
         ),
-        body: Text('$counter'),
+        body: ImageList(images),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
-          onPressed: () {
-            setState(() {
-              counter++;
-            });
-          },
+          onPressed: fetchImage,
         ),
       )
     );
---
+++ examples1/pics/lib/src/models/image_model.dart
@@ -0,0 +1,15 @@
+class ImageModel {
+
+  int id;
+  String url;
+  String title;
+
+  ImageModel(this.id, this.url, this.title);
+
+  ImageModel.fromJson(Map<String, dynamic> parsedJson) {
+    this.id = parsedJson['id'];
+    this.url = parsedJson['url'];
+    this.title = parsedJson['title'];
+  }
+
+}
