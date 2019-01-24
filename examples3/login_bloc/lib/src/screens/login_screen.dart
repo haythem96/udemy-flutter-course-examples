661a44c591aa8e38871d0b76ae88fb1549ed5445
Haythem Sellami <sellamihaythem96@gmail.com>
2019-01-25 00:32:51 +0100

    BLOC Pattern

---
+++ examples3/login_bloc/lib/src/screens/login_screen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+
+class LoginScreen extends StatelessWidget {
+
+  Widget build(context) {
+    return Container(
+      margin: EdgeInsets.all(20.5),
+      child: Column(
+        children: <Widget>[
+          emailField(),
+          passwordField(),
+          Container(margin: EdgeInsets.only(top: 25.0)),
+          submitButton()
+        ],
+      ),
+    );
+  }
+
+  Widget emailField() {
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        hintText: "you@example.com",
+        labelText: "Email Address"
+      ),
+    );
+  }
+
+  Widget passwordField() {
+    return TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: "password",
+        labelText: "Password"
+      ),
+    );
+  }
+
+  Widget submitButton() {
+    return RaisedButton(
+      child: Text("Login"),
+      color: Colors.blue,
+      onPressed: () {
+
+      },
+    );
+  }
+}
