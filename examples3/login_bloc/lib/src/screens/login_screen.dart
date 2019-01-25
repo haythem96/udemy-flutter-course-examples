35213ca214bb2f343cbab88763ce69dc6bf0fd5c
Haythem Sellami <sellamihaythem96@gmail.com>
2019-01-25 13:17:29 +0100

    block scoped instance

--- examples3/login_bloc/lib/src/screens/login_screen.dart
+++ examples3/login_bloc/lib/src/screens/login_screen.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
-
+import '../blocs/bloc.dart';
+import '../blocs/provider.dart';
 
 class LoginScreen extends StatelessWidget {
 
   Widget build(context) {
+    final bloc = Provider.of(context);
+
     return Container(
       margin: EdgeInsets.all(20.5),
       child: Column(
         children: <Widget>[
-          emailField(),
-          passwordField(),
+          emailField(bloc),
+          passwordField(bloc),
           Container(margin: EdgeInsets.only(top: 25.0)),
           submitButton()
         ],
@@ -17,23 +20,37 @@ class LoginScreen extends StatelessWidget {
     );
   }
 
-  Widget emailField() {
-    return TextField(
-      keyboardType: TextInputType.emailAddress,
-      decoration: InputDecoration(
-        hintText: "you@example.com",
-        labelText: "Email Address"
-      ),
+  Widget emailField(Bloc bloc) {
+    return StreamBuilder(
+      stream: bloc.email,
+      builder: (context, snapshot) {
+        return TextField(
+          onChanged: bloc.changeEmail,
+          keyboardType: TextInputType.emailAddress,
+          decoration: InputDecoration(
+            hintText: "you@example.com",
+            labelText: "Email Address",
+            errorText: snapshot.error
+          ),
+        );
+      },
     );
   }
 
-  Widget passwordField() {
-    return TextField(
-      obscureText: true,
-      decoration: InputDecoration(
-        hintText: "password",
-        labelText: "Password"
-      ),
+  Widget passwordField(Bloc bloc) {
+    return StreamBuilder(
+      stream: bloc.password,
+      builder: (context, snapshot) {
+        return TextField(
+          onChanged: bloc.changePassword,
+          obscureText: true,
+          decoration: InputDecoration(
+            hintText: "password",
+            labelText: "Password",
+            errorText: snapshot.error
+          ),
+        );
+      },
     );
   }
 
