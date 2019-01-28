d7df0c9bebf0ef15700b3eb51bad02472c375131
Haythem Sellami <sellamihaythem96@gmail.com>
2019-01-28 15:25:54 +0100

    rxDart

--- examples3/login_bloc/lib/src/screens/login_screen.dart
+++ examples3/login_bloc/lib/src/screens/login_screen.dart
@@ -14,7 +14,7 @@ class LoginScreen extends StatelessWidget {
           emailField(bloc),
           passwordField(bloc),
           Container(margin: EdgeInsets.only(top: 25.0)),
-          submitButton()
+          submitButton(bloc)
         ],
       ),
     );
@@ -54,12 +54,15 @@ class LoginScreen extends StatelessWidget {
     );
   }
 
-  Widget submitButton() {
-    return RaisedButton(
-      child: Text("Login"),
-      color: Colors.blue,
-      onPressed: () {
-
+  Widget submitButton(Bloc bloc) {
+    return StreamBuilder(
+      stream: bloc.submitValid,
+      builder: (context, snapshot) {
+        return RaisedButton(
+          child: Text("Login"),
+          color: Colors.blue,
+          onPressed: snapshot.hasData ? bloc.submit : null,
+        );
       },
     );
   }
